2125548009eaa63818b197ebccc22dd7fc6d68fa
Vladislav <lowkkidvstheworld@gmail.com>
2024-06-15 16:28:29 +0300

    init project

---
+++ src/test/java/com/example/restaurant_vote_system/RestaurantVoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_vote_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
